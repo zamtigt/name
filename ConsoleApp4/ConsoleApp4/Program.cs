147ffd632521256c3b016d7bd07dbe44c3cd694f
zamtigt <zamtigt@gmail.com>
2022-02-27 17:14:33 +0900

    name

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 7;
+
+            for (int y = 0; y < number; y += 2)
+            {
+                for(int x = 0; x < y/2; x++)
+                {
+                    Console.Write(".");
+                }
+
+                for (int x = 0; x < (number+1)-(y+1)-1; x++)
+                {
+                    Console.Write("*");
+                }
+                Console.Write("$");
+                Console.WriteLine();
+            }
+        }
+    }
+}
